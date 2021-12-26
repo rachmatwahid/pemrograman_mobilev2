4fcb8c71febff5b60db188bc136867ca26daa56f
rachmatwahid <32413014+rachmatwahid@users.noreply.github.com>
2021-12-26 08:15:58 +0700

    initial commit

--- 05/Resto/app/src/main/java/com/rachmatwahid/resto/MenuActivity.java
+++ 05/Resto/app/src/main/java/com/rachmatwahid/resto/MenuActivity.java
@@ -6,13 +6,36 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class MenuActivity extends AppCompatActivity {
+
+    TextView textViewMixedRice, textViewFriedRice, textViewNoodle, textViewMeatball, textViewFriedChicken, textViewTotal;
+
+    HashMap<String, Integer> price = new HashMap();
+
+    Integer totalPrice = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        price.put("mixedRice", 10000);
+        price.put("friedRice", 12000);
+        price.put("noodle", 9500);
+        price.put("meatball", 14000);
+        price.put("friedChicken", 13500);
+
+        textViewMixedRice = findViewById(R.id.textViewMixedRice);
+        textViewFriedRice = findViewById(R.id.textViewFriedRice);
+        textViewNoodle = findViewById(R.id.textViewNoodle);
+        textViewMeatball = findViewById(R.id.textViewMeatball);
+        textViewFriedChicken = findViewById(R.id.textViewFriedChicken);
+        textViewTotal = findViewById(R.id.textViewTotal);
+
     }
 
     public void launchInstagram(View view) {
@@ -32,5 +55,21 @@ public class MenuActivity extends AppCompatActivity {
         if (twitterIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(twitterIntent);
         }
+    }
+
+    public void selectDishes(View view) {
+        switch (view.getId()) {
+            case R.id.textViewMixedRice:
+                totalPrice += price.get("mixedRice");
+            case R.id.textViewFriedRice:
+                totalPrice += price.get("friedRice");
+            case R.id.textViewNoodle:
+                totalPrice += price.get("noodle");
+            case R.id.textViewMeatball:
+                totalPrice += price.get("meatball");
+            case R.id.textViewFriedChicken:
+                totalPrice += price.get("friedChicken");
+        }
+        textViewTotal.setText(totalPrice);
     }
 }
